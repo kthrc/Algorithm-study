fe6f07f547f039ba678aa92b9d48902fd1ea84c7
Yujung Choi <106314022+kthrc@users.noreply.github.com>
2025-06-15 20:32:29 +0900

    Update 체스판 다시 칠하기.py

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -8,7 +8,7 @@ for i in range(n - 7):
     for j in range(m - 7):
         cnt1 = 0  # W로 시작하는 경우
         cnt2 = 0  # B로 시작하는 경우
-        for x in range(8):     # ← 이 부분이 누락됐던 부분!
+        for x in range(8):    
             for y in range(8):
                 curr = board[i + x][j + y]
                 if (x + y) % 2 == 0:  # 짝수 위치
