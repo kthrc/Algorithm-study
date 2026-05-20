0ea4ea9900038b3a9522824528d6ecc76a044ecc
Yujung Choi <106314022+kthrc@users.noreply.github.com>
2026-05-21 04:00:38 +0900

    [level 1] Title: 예산, Time: 0.06 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/1/Python3/12982. 예산/예산.py
@@ -0,0 +1,16 @@
+def solution(d, budget):
+    answer = 0
+    
+    d.sort()
+    
+    for i in d:
+        if i <= budget:
+            budget -= i
+            answer += 1
+        else:
+            break
+            
+    return answer 
+
+    
+    
