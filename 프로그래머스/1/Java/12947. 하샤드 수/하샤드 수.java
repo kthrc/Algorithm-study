93f89f810b4d65db194aa32a39b9ce0d566dca2c
Yujung Choi <106314022+kthrc@users.noreply.github.com>
2026-08-29 01:28:34 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/Java/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+
+        String str = String.valueOf(x);
+
+        for (int i = 0; i < str.length(); i++) {
+            sum += str.charAt(i) - '0';
+        }
+
+        return x % sum == 0;
+    }
+}
